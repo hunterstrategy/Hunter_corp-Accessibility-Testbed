5376643bd09b32246df1571195ae06a5ac25e456
Ben Jerauld <ben.jerauld@hunterstrategy.net>
2020-08-21 08:06:49 -0400

    Included routing for site, added a presentation table version of table match

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,23 +3,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `
-<mat-toolbar>
-  <span>Accessibility Testbed</span>
-  <span class="example-spacer"></span>
-  <span class="example-spacer"></span>
-  <button mat-icon-button class="example-icon" aria-label="home">
-    <mat-icon>home</mat-icon>
-  </button>
-  <button mat-icon-button class="example-icon" aria-label="table">
-    <mat-icon>table</mat-icon>
-  </button>
-  <button mat-icon-button class="example-icon" aria-label="map">
-    <mat-icon>map</mat-icon>
-  </button>
-</mat-toolbar>
-  `,
-  styles: []
+  templateUrl: 'header.component.html',
+  styleUrls: ['header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
